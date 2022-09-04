a8be108f46962f55c406bd4ff5f4af0a95b3cc3b
Zachary Morris <zachary-j-morris@uiowa.edu>
2022-09-04 23:08:40 +0000

    ENH: Adding the first files to the repository

---
+++ lib/ruby_intro.rb
@@ -0,0 +1,40 @@
+# When done, submit this entire file to the autograder.
+
+# Part 1
+<<<<<<< HEAD
+# 
+
+=======
+
+def sum arr
+  # YOUR CODE HERE
+end
+
+def max_2_sum arr
+  # YOUR CODE HERE
+end
+
+def sum_to_n? arr, n
+  # YOUR CODE HERE
+end
+>>>>>>> 0eedd870e5fc03e6bd83de9391a6d8844ebc8deb
+
+# Part 2
+
+def hello(name)
+  # YOUR CODE HERE
+end
+
+def starts_with_consonant? s
+  # YOUR CODE HERE
+end
+
+def binary_multiple_of_4? s
+  # YOUR CODE HERE
+end
+
+# Part 3
+
+class BookInStock
+# YOUR CODE HERE
+end
